3afd2b5f62307738b1bb6aed601bbbca9e21d50b
Elias-Git <mr.elias2018@gmail.com>
2022-11-16 12:15:17 +0600

    Banner start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {RouterProvider} from "react-router-dom";
 import router from './Routes/Routes/Routes';
 function App() {
   return (
-    <div >
+    <div className='max-w-[1440px] mx-auto'>
        <RouterProvider router={router}>
 
        </RouterProvider>

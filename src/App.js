d68e9230fa5ce513c340314a4e53be03d6496b47
Elias-Git <mr.elias2018@gmail.com>
2022-11-16 11:25:06 +0600

    route setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-
+import {RouterProvider} from "react-router-dom";
+import router from './Routes/Routes/Routes';
 function App() {
   return (
-    <div className="App">
-     <button className="btn btn-info">Info</button>
-<button className="btn btn-success">Success</button>
-<button className="btn btn-warning">Warning</button>
-<button className="btn btn-error">Error</button>
+    <div >
+       <RouterProvider router={router}>
+
+       </RouterProvider>
+    
     </div>
   );
 }

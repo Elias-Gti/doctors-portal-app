c007cec8441307ba716bc48a49a7a02692510a92
Elias-Git <mr.elias2018@gmail.com>
2022-11-18 22:06:33 +0600

    doctor start

--- src/Pages/Home/MakeAppointment/MakeAppointment.js
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import bgImg from '../../../assets/images/appointment.png';
 import drImg from '../../../assets/images/doctor.png';
+import './MakeAppointment.css'
 
 const MakeAppointment = () => {
     return (
-        <section className=' mt-20 rounded-lg' style={{ backgroundImage: `url(${bgImg})` }}>
+        <section className=' mt-36 rounded-lg' style={{ backgroundImage: `url(${bgImg})` }}>
 
             <div className="hero">
             <div className="hero-content flex-col lg:flex-row">
@@ -12,7 +13,7 @@ const MakeAppointment = () => {
                 <div className='lg:w-1/2 text-white'>
                     <h1 className="text-5xl font-bold">Box Office News!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="font-bold btn btn-primary text-white bg-gradient-to-r from-primary to-secondary">Get Started</button>
                 </div>
             </div>
         </div>
